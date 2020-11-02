842f94227501e611d049c26259accff61c5742f3
Globo97 <gabirubin@gmail.com>
2020-11-02 17:00:23 -0300

    1er avance de maqueta carrito

--- app.js
+++ app.js
@@ -8,10 +8,23 @@ app.listen(3000, ()=>{
 
 app.use(express.static(__dirname + "/public"));
 
+
+
+// Cart and derives (gabi)
 app.get("/carritoDeCompras",(req,res)=>{
     res.sendFile(path.resolve(__dirname,"views","productCart.html"))
 })
 
-app.get("*",(req,res=>{
+app.get("/productosGuardados",(req,res)=>{
+    res.sendFile(path.resolve(__dirname,"views","savedProducts.html"))
+})
+
+app.get("/favoritos",(req,res)=>{
+    res.sendFile(path.resolve(__dirname,"views","favourites.html"))
+})
+
+
+// Default route
+app.get("*",(req,res)=>{
     res.status(404).send("Error 404 not found")
-}));
+});
