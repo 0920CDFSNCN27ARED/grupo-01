b6de001a84e8e9dc66c164a878332cb84c4081d4
Vpriu <maria.v.priu@hotmail.con>
2020-11-18 23:47:12 -0300

    Producto Desktop y Productos General

--- app.js
+++ app.js
@@ -2,7 +2,7 @@ const express = require("express");
 const app = express();
 const path = require("path");
 
-app.listen(3000, ()=>{
+app.listen(3000, () => {
     console.log("Server running in port 3000");
 });
 
@@ -10,31 +10,33 @@ const staticFileRouter = express.static("public");
 app.use(staticFileRouter);
 
 //home
-app.get("/",(req,res)=>{
-    res.sendFile(path.resolve(__dirname,"views/index.html"))
-})
+app.get("/", (req, res) => {
+    res.sendFile(path.resolve(__dirname, "views/index.html"));
+});
 
 //product
 app.get("/productDetail", (req, res) => {
-    res.sendFile(path.resolve(__dirname, "views/product.html"))
-})
+    res.sendFile(path.resolve(__dirname, "views/product.html"));
+});
 
+app.get("/products", (req, res) => {
+    res.sendFile(path.resolve(__dirname, "views/products.html"));
+});
 
 // Cart and derives (gabi)
-app.get("/carritoDeCompras",(req,res)=>{
-    res.sendFile(path.resolve(__dirname,"views/productCart.html"))
-})
+app.get("/carritoDeCompras", (req, res) => {
+    res.sendFile(path.resolve(__dirname, "views/productCart.html"));
+});
 
-app.get("/productosGuardados",(req,res)=>{
-    res.sendFile(path.resolve(__dirname,"views/savedProducts.html"))
-})
+app.get("/productosGuardados", (req, res) => {
+    res.sendFile(path.resolve(__dirname, "views/savedProducts.html"));
+});
 
-app.get("/favoritos",(req,res)=>{
-    res.sendFile(path.resolve(__dirname,"views/favourites.html"))
-})
-
+app.get("/favoritos", (req, res) => {
+    res.sendFile(path.resolve(__dirname, "views/favourites.html"));
+});
 
 // Default route
-app.get("*",(req,res)=>{
-    res.status(404).send("Error 404 not found")
+app.get("*", (req, res) => {
+    res.status(404).send("Error 404 not found");
 });
