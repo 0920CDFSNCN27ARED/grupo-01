d69256226ea4e9505c722740a32dae5b6c1c12e5
Veronica <verogenoni@hotmail.com>
2021-01-10 16:35:42 -0300

    Arregle algunas cosas del CSS

--- routes/productsRoutes.js
+++ routes/productsRoutes.js
@@ -1,15 +1,15 @@
 const express = require("express");
 const router = express.Router();
 const multer = require("multer");
-const upload = multer({dest: "public/images"});
+const upload = multer({ dest: "public/images" });
 
 const productsController = require("../controllers/productsController");
 
 router.get("/", productsController.showAll);
 router.get("/crear", productsController.newProduct);
-router.post("/crear",upload.single("image"),productsController.createProduct)
+router.post("/crear", upload.single("image"), productsController.createProduct);
 
 router.get("/:id", productsController.showOne);
-router.get("/:id/edit", productsController.editProduct);
+router.get("/:id/editar", productsController.editProduct);
 
 module.exports = router;
