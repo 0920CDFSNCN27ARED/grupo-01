a9bc935a82c3c918cd53eb865815fc0aae3220c8
Globo97 <gabirubin@gmail.com>
2021-01-27 21:21:22 -0300

    add editar button

--- middlewares/isAdmin.js
+++ middlewares/isAdmin.js
@@ -1,5 +1,5 @@
 function isAdmin(req, res, next) {
-    if (res.app.locals.user.category != "empresa") {
+    if (res.app.locals.user !== null && res.app.locals.user.category != "empresa") {
         res.redirect("/");
     } else if (res.app.locals.user == null) {
         res.redirect("/");
