a424923ce374b2f54d4a5c4c870d28d5bfcbe9b9
Globo97 <gabirubin@gmail.com>
2021-02-27 19:47:15 -0300

    signup validation funcionando sin mensajes ni colores

--- public/js/validateSignup.js
+++ public/js/validateSignup.js
@@ -1,53 +1,87 @@
 const form = document.querySelector("form");
-const firstName = document.getElementById("firstName");
-const lastName = document.getElementById("lastName");
-const dni = document.getElementById("dni");
-const email = document.getElementById("email");
-const password = document.getElementById("password");
-const avatar = document.getElementById("avatar");
+
+// VALIDATION VARS
+const noNumberValidation = [
+    validator.isAlpha,
+    ["en-US"],
+    "El campo debe estar compuesto unicamente por letras, no numeros",
+];
+
+const isLength = (minNmbr, maxNmbr) => {
+    let options;
+    let errorMsg;
+    let validationArray;
+
+    if (minNmbr && maxNmbr) {
+        options = { min: minNmbr, max: maxNmbr };
+        errorMsg = `El campo debe tener minimo ${options.min} caracter/es y maximo ${options.max} `;
+    } else if (minNmbr) {
+        options = { min: minNmbr };
+        errorMsg = `El campo debe tener minimo ${options.min} caracter/es `;
+    } else if (maxNmbr) {
+        options = { min: minNmbr };
+        errorMsg = `El campo debe tener maximo ${options.max} caracter/es`;
+    }
+    validationArray = [validator.isLength, options, errorMsg];
+    return validationArray;
+};
+const termsAccepted = [
+    validator.equals,
+    "true",
+    "Debes aceptar los terminos y condiciones",
+];
+
+///////////////////////
 
 form.addEventListener("submit", (event) => {
     event.preventDefault();
-    validateField("firstName", [
-        [
-            validator.isLength,
-            { min: 3 },
-            "El campo debe tener al menos dos caracteres",
-        ],
-        [
-            validator.isAlpha,
-            ["en-US"],
-            "El campo debe contener solo letras y no numeros",
-        ],
+    validateMultipleFields([
+        ["firstName", [isLength(2), noNumberValidation]],
+        ["lastName", [isLength(2), noNumberValidation]],
+        ["dni", [isLength(7, 8)]],
+        ["email", [[validator.isEmail,[null],"Debes ingresar un Email valido"]]],
+        ["password", [isLength(8)]],
+        // ["avatar", [isLength(2), noNumberValidation]],
+        ["terms", [termsAccepted]],
     ]);
 });
 
-function validateField(inputId, validations) {
-    let errors = [];
-    const input = document.getElementById(inputId);
+/////////  FUNCTIONS
 
-    for (const validation of validations) {
-        let validationFunction = validation[0];
-        let errorMsg = validation[validation.length - 1];
-        let options = validation.length > 2 ? validation[1] : null;
+function validateMultipleFields(fieldsValidation) {
+    fieldsValidation.forEach((fieldValidation) => {
+        const inputId = fieldValidation[0];
+        const validations = fieldValidation[1];
 
-        if (!validationFunction(input.value, options)) {
-            errors.push(errorMsg);
-            input.classList.remove("is-valid");
-            input.classList.add("is-not-valid");
-            console.log(errors);
+        let errors = [];
+        let input = document.getElementById(inputId);
+        switch (input.type) {
+            case "checkbox":
+                inputValue = input.checked.toString();
+                break;
+            default:
+                inputValue = input.value;
         }
-    }
-    if (errors.length === 0) {
-        input.classList.remove("is-not-valid");
-        return input.classList.add("is-valid");
-    }
-    return input.classList.add("is-not-valid");
+        for (const validation of validations) {
+            let validationFunction = validation[0];
+            let errorMsg = validation[validation.length - 1];
+            let options = validation.length > 2 ? validation[1] : null;
+
+            if (!validationFunction(inputValue, options)) {
+                errors.push(errorMsg);
+                input.classList.remove("is-valid");
+                input.classList.add("is-not-valid");
+                console.log(errors);
+            }
+        }
+        if (errors.length === 0) {
+            input.classList.remove("is-not-valid");
+            input.classList.add("is-valid");
+        }
+        // input.classList.add("is-not-valid");
+    });
 }
 
-// Nombre: min de 2 caracteres, no contener nros
-// Apellido: min de 2 car, no contener nros
-//DNI: solo contener nros, min 7, max 8
 // Email: ser valido, no repetirse con otros mails
 // password: al menos 8 caracteres, opc(mayus, nros, etc)
 // imagen: archivo valido(jpg, jpeg, png, gif)
