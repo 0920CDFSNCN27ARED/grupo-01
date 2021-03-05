a327b1014b66f799b950a780a41358e869b7aba4
Globo97 <gabirubin@gmail.com>
2021-03-05 15:14:55 -0300

    cart boxes de cantidades funcionando

--- site/public/js/cart.js
+++ site/public/js/cart.js
@@ -1,13 +1,16 @@
+const productCards = document.getElementsByClassName("product-card");
+const totalPrice = document.getElementById("total-price");
+console.log(totalPrice);
 
-const article = document.getElementsByTagName("article")
+Array.from(productCards).forEach((productCard) => {
+    const quantity = productCard.querySelector("#quantity");
+    const partialPrice = productCard.querySelector("#partial-price");
+    const unityPrice = partialPrice.innerText.split("$")[1];
 
-
-const quantity = document.getElementById("quantity");
-const productPrice = document.getElementById("");
-const partialPrice = document.getElementById("partial-price");
-
-const unityPrice = partialPrice.innerText.split("$")[1];
-
-quantity.addEventListener("change", (event) => {
-    partialPrice.innerText = `$${event.target.value * unityPrice}`;
+    quantity.addEventListener("change", (event) => {
+        partialPrice.innerText = `$${event.target.value * unityPrice}`;
+        totalPrice.innerText = `$${
+            Number(totalPrice.innerText.split("$")[1]) + Number(unityPrice)
+        }`;
+    });
 });
