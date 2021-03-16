cda6c08fcfcb03af440e1cdf493415184c0aaa96
Globo97 <gabirubin@gmail.com>
2021-03-16 15:06:50 -0300

    Creacion de orderItems andando

--- site/src/controllers/api/ordersController.js
+++ site/src/controllers/api/ordersController.js
@@ -1,22 +1,37 @@
-const { Order, Product } = require("../../database/models");
+const { Order, Product, OrderItem } = require("../../database/models");
 
 module.exports = {
     createOrder: async (req, res) => {
         const cart = req.body;
         const userId = res.locals.user.id;
-        ///// Get totalPrice
-        let totalPrice = 0;
-        for (const cartProd of cart) {
-            const fullProd = await Product.findByPk(cartProd.id);
-            totalPrice += Number(fullProd.price * cartProd.quantity);
-        }
+        const lastOrderId = await Order.findAll({
+            order: [["id", "DESC"]],
+        });
+        console.log(lastOrderId[0]);
         ////Check if order exists
         const existingOrder = await Order.findOne({
             where: {
                 buyerUserId: userId,
             },
         });
-    
+
+        ///// Get totalPrice
+        let totalPrice = 0;
+        for (const cartProd of cart) {
+            const fullProd = await Product.findByPk(cartProd.id);
+            totalPrice += Number(fullProd.price * cartProd.quantity);
+            await OrderItem.create({
+                subtotal: fullProd.price * cartProd.quantity,
+                quantity: cartProd.quantity,
+                price: fullProd.price,
+                orderId: existingOrder
+                    ? existingOrder.id
+                    : lastOrderId[0].id + 1,
+                productId: cartProd.id,
+                discount: 0,
+            });
+        }
+
         if (existingOrder) {
             await Order.update(
                 {
