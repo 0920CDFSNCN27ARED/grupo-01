f49352dc19a827405aee88b5de20c92c368a9676
Globo97 <gabirubin@gmail.com>
2021-03-20 20:12:40 -0300

    Updates in order controller

--- site/src/controllers/api/ordersController.js
+++ site/src/controllers/api/ordersController.js
@@ -4,34 +4,20 @@ module.exports = {
     createOrder: async (req, res) => {
         const cart = req.body;
         const userId = res.locals.user.id;
-        const lastOrderId = await Order.findAll({
+        let lastOrderId = await Order.findAll({
             order: [["id", "DESC"]],
         });
-        console.log(lastOrderId[0]);
         ////Check if order exists
         const existingOrder = await Order.findOne({
             where: {
                 buyerUserId: userId,
             },
         });
+        // Get new orderId
+        const newOrderId = lastOrderId[0] ? lastOrderId[0] : 1;
+        const orderId = existingOrder ? existingOrder.id : newOrderId;
 
-        ///// Get totalPrice
-        let totalPrice = 0;
-        for (const cartProd of cart) {
-            const fullProd = await Product.findByPk(cartProd.id);
-            totalPrice += Number(fullProd.price * cartProd.quantity);
-            await OrderItem.create({
-                subtotal: fullProd.price * cartProd.quantity,
-                quantity: cartProd.quantity,
-                price: fullProd.price,
-                orderId: existingOrder
-                    ? existingOrder.id
-                    : lastOrderId[0].id + 1,
-                productId: cartProd.id,
-                discount: 0,
-            });
-        }
-
+        // Create/Update order 
         if (existingOrder) {
             await Order.update(
                 {
@@ -44,7 +30,6 @@ module.exports = {
                     },
                 }
             );
-            return res.send(existingOrder);
         }
 
         await Order.create({
@@ -52,6 +37,25 @@ module.exports = {
             addressId: 1,
             total: totalPrice,
         });
+        ///// Get totalPrice
+        
+        let totalPrice = 0;
+        for (const cartProd of cart) {
+            const fullProd = await Product.findByPk(cartProd.id);
+            totalPrice += Number(fullProd.price * cartProd.quantity);
+
+            //Create orderItems
+            await OrderItem.create({
+                subtotal: fullProd.price * cartProd.quantity,
+                quantity: cartProd.quantity,
+                price: fullProd.price,
+                orderId: orderId,
+                productId: cartProd.id,
+                discount: fullProd.discount,
+            });
+            return res.send(existingOrder);
+        }
+
         res.send({ msg: "Orden creada" });
     },
 };
