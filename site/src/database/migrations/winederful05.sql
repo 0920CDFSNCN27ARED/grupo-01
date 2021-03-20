8dfe92dce5e7ee61a074e8d50a3e07d11d2c956a
Globo97 <gabirubin@gmail.com>
2021-03-20 17:06:10 -0300

    database working with grapes

--- site/src/database/migrations/winederful05.sql
+++ site/src/database/migrations/winederful05.sql
@@ -1,42 +1,15 @@
--- -----------------------------------------------------
--- Table `winederful`.`products`
--- -----------------------------------------------------
-DROP TABLE IF EXISTS `winederful`.`products` ;
-
-CREATE TABLE IF NOT EXISTS `winederful`.`products` (
-  `id` INT(11) NOT NULL AUTO_INCREMENT,
-  `productName` VARCHAR(45) NULL DEFAULT NULL,
-  `description` VARCHAR(60000) NULL DEFAULT NULL,
-  `year` VARCHAR(45) NULL DEFAULT NULL,
-  `aged` VARCHAR(45) NULL DEFAULT NULL,
-  `temperature` VARCHAR(45) NULL DEFAULT NULL,
-  `price` DECIMAL(10,0) NULL DEFAULT NULL,
-  `stock` INT(11) NULL DEFAULT NULL,
-  `image` VARCHAR(655) NULL DEFAULT NULL,
-  `discount` DECIMAL(10,0) NULL DEFAULT NULL,
-  `cellarUserId` INT(11) NOT NULL,
-  `createdAt` TIMESTAMP NULL DEFAULT NULL,
-  `updatedAt` TIMESTAMP NULL DEFAULT NULL,
-  `deletedAt` TIMESTAMP NULL DEFAULT NULL,
-  `grapesId` INT NOT NULL,
-  PRIMARY KEY (`id`),
-  UNIQUE INDEX `productName_UNIQUE` (`productName` ASC) VISIBLE,
-  INDEX `fk_products_cellar_users1_idx` (`cellarUserId` ASC) VISIBLE,
-  INDEX `fk_products_grapes1_idx` (`grapesId` ASC) VISIBLE,
-  CONSTRAINT `fk_products_cellar_users1`
-    FOREIGN KEY (`cellarUserId`)
-    REFERENCES `winederful`.`cellar_users` (`id`),
-  CONSTRAINT `fk_products_grapes1`
-    FOREIGN KEY (`grapesId`)
-    REFERENCES `mydb`.`grapes` (`id`)
-    ON DELETE NO ACTION
-    ON UPDATE NO ACTION)
-ENGINE = InnoDB
-AUTO_INCREMENT = 28
-DEFAULT CHARACTER SET = latin1;
-
-
-CREATE TABLE `winederful`.`grapes` (
-  `id` INT NOT NULL AUTO_INCREMENT,
-  `name` VARCHAR(45) NULL,
-  PRIMARY KEY (`id`));
+insert into grapes (id, name) values (1, 'Saxifragaceae');
+insert into grapes (id, name) values (2, 'Cyperaceae');
+insert into grapes (id, name) values (3, 'Fagaceae');
+insert into grapes (id, name) values (4, 'Scrophulariaceae');
+insert into grapes (id, name) values (5, 'Scrophulariaceae');
+insert into grapes (id, name) values (6, 'Dipsacaceae');
+insert into grapes (id, name) values (7, 'Fabaceae');
+insert into grapes (id, name) values (8, 'Rubiaceae');
+insert into grapes (id, name) values (9, 'Teloschistaceae');
+insert into grapes (id, name) values (10, 'Cyperaceae');
+insert into grapes (id, name) values (11, 'Scrophulariaceae');
+insert into grapes (id, name) values (12, 'Asteraceae');
+insert into grapes (id, name) values (13, 'Cladoniaceae');
+insert into grapes (id, name) values (14, 'Typhaceae');
+insert into grapes (id, name) values (15, 'Oleaceae');
